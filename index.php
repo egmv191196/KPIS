3c939f2bcfa8e1486c2836460df291e4c7a56f53
unknown <edgargabrielv96@gmail.com>
2021-04-13 11:39:42 -0500

    Metodos en un solo archivo

--- index.php
+++ index.php
@@ -10,28 +10,29 @@
         <link rel="stylesheet" href="./css/style.css" >
     </head>
     <body >
-    <?php include("./src/Views/modal.php");?>
         <div class="container ">
             <div class="row justify-content-center pt-5 mt-5 ">
                 <div class="col-md-4 formulario ">
-                    <form action="./src/Script/login.php" method="POST">  
+                    <form id="formLogin" method="POST">  
                         <div class="form-group text-center">
                             <img src="./src/Img/logo.png" class="img-fluid mt-3" alt="Responsive image">
                             <h1 class="text-center" >Iniciar Sesion</h1>
                             <label for="exampleInputEmail1">Ingresar tu usuario</label>
-                            <input type="text" name="user" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar Usuario">
+                            <input type="text" name="user" class="form-control " id="user" aria-describedby="emailHelp" placeholder="Ingresar Usuario">
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Contraseña</label>
-                            <input type="password" name="passwd" class="form-control " id="exampleInputPassword1" placeholder="Contraseña">
-                            <button type="submit" class="btn btn-primary mt-3 center ">Iniciar Sesion</button>
+                            <input type="password" name="passwd" class="form-control " id="passwd" placeholder="Contraseña">
+                            <button class="btn btn-primary mt-3 center" id="signIn">Iniciar Sesion</button>
                         </div>  
                     </form> 
                 </div>
             </div>
         </div>  
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+        <script src="./js/Metodos.js"></script>
     </body>
 </html>
+
---
+++ js/Metodos.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    //Login
+    $('#signIn').click(function(){
+        var datos=$('#formLogin').serialize();
+        $.ajax({
+            type: "POST",
+            url: "./src/Script/login.php",
+            data: datos,
+        }).done(function(response){      
+            if(response == 1){
+                location.href ="./src/Views/InicioGG.php";
+            }else if(response == 2){
+                location.href ="./src/Views/InicioGT.php";
+            }else if(response == 3){
+                location.href ="./src/Views/InicioGC.php";
+            }else if(response == 0){
+                alert("Contraseña erronea");
+            }else if(response == 4){
+                alert("No existe el usuario");
+            }
+        }).fail(function(response){
+            alert("Hubo un error en el server, reintentelo de nuevo");
+        });
+        return false;
+    });
+    //Agregar Clientes
+    $('#AddCliente').click(function(){
+        var datos=$('#frmCliente').serialize();
+        $.ajax({
+            type: "POST",
+            url: "../Script/insertarClientes.php",
+            data: datos,
+        }).done(function(response){
+        if(response == 1){
+            alert("Usuario agregado correctamente");
+            window.location="./Clientes.php";
+        }else{
+            console.log("no es 1"+response);
+        }
+    }).fail(function(response){
+        console.log("error"+response);
+    });
+        return false;
+    });
+    //Agregar Proveedor
+    $('#addPro').click(function(){
+        var datos=$('#frmProv').serialize();
+        $.ajax({
+            type: "POST",
+            url: "../Script/addProveedor.php",
+            data: datos,
+        }).done(function(response){
+        if(response == 1){
+            alert("Usuario agregado correctamente");
+            window.location="./Proveedor.php";
+        }else{
+            console.log("error al agregar "+ response);
+        }
+    }).fail(function(response){
+        console.log("error"+response);
+    });
+        return false
+    });
+});
